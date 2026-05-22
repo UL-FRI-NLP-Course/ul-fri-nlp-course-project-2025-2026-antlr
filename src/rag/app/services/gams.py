7ef7ca3877db941943ff4708e2159460f004c34b
samolego <34912839+samolego@users.noreply.github.com>
2026-05-22 11:34:30 +0200

    minor code format changes

--- src/rag/app/services/gams.py
+++ src/rag/app/services/gams.py
@@ -1,2 +1,3 @@
 from services.llm_models import GaMS
-MODEL_OBJECT = GaMS() # gams singleton: to use, import MODEL_OBJECT
+
+MODEL_OBJECT = GaMS()
--- src/rag/app/services/llm_models.py
+++ src/rag/app/services/llm_models.py
@@ -1,15 +1,17 @@
-from transformers import pipeline, logging
 import math
-# logging.set_verbosity_debug() # uncomment to make transformers more verbose
+
+from transformers import logging, pipeline
+
 
 # model superclass (each model should implement this)
 class LLModel:
     def __init__(self, params):
         self.params = params
-    
+
     # abstract method
     def get_response(self, message):
         raise NotImplementedError
+
 
 # gams
 class GaMS(LLModel):
@@ -20,36 +22,39 @@ class GaMS(LLModel):
         self.pline = pipeline(
             "text-generation",
             model=self.params.model_id,
-            device_map=self.params.torch_device
+            device_map=self.params.torch_device,
         )
-    
+
     def get_response(self, messages, temperature=None):
         temp = self.params.temperature
-        lowest_temp=0.001
-        if(temperature is not None):
-            temp = temperature # use different temperature
-            if(temperature > 1.0):
-                print("[warn] GaMS.get_response: temperature value larger than 1.0, using 1.0.")
+        lowest_temp = 0.001
+        if temperature is not None:
+            temp = temperature  # use different temperature
+            if temperature > 1.0:
+                print(
+                    "[warn] GaMS.get_response: temperature value larger than 1.0, using 1.0."
+                )
                 temp = 1.0
-            if(math.isclose(temperature, 0.0) or temperature < 0.0):
-                print("[warn] GaMS.get_response: temperature value smaller than 0.0 or equal to 0.0, using lowest_temp value (%.3f)."%lowest_temp)
+            if math.isclose(temperature, 0.0) or temperature < 0.0:
+                print(
+                    "[warn] GaMS.get_response: temperature value smaller than 0.0 or equal to 0.0, using lowest_temp value (%.3f)."
+                    % lowest_temp
+                )
                 temp = lowest_temp
-            
+
         response = self.pline(
             messages,
             max_new_tokens=self.params.max_new_tokens,
             do_sample=True,
-            temperature=temp
-            )
+            temperature=temp,
+        )
         return response
+
 
 # gams params
 class GaMSParams:
     def __init__(self):
         self.model_id = "GaMS-Beta/GaMS-9B-Instruct-Nemotron"
-        self.torch_device = "cuda" # "cpu" # <- cpu takes forever
-        self.max_new_tokens=512
-        self.temperature=0.1
-
-
-
+        self.torch_device = "cuda"
+        self.max_new_tokens = 512
+        self.temperature = 0.1
